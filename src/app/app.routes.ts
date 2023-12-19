667aa82c462a8a0a0a772c0afba968c45a892e7e
codinglamer <ser.mac555@gmail.com>
2023-12-19 17:17:20 +0300

    routing configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { TaskListPageComponent } from "./pages/task-list-page/task-list-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { SignUpPageComponent } from "./pages/sign-up-page/sign-up-page.component";
 
-export const routes: Routes = [];
+const appTitle = "Todoist";
+
+export const routes: Routes = [
+  {path: "", title: appTitle, component: TaskListPageComponent, pathMatch: "full"},
+  {path: "login", title: `${appTitle} | Login`, component: LoginPageComponent},
+  {path: "signup", title: `${appTitle} | Sign up`, component: SignUpPageComponent},
+  {path: "**", redirectTo: ""}
+];
