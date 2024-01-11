5be231a321f35f33485a61e53d9b0cd4957bdbb7
codinglamer <ser.mac555@gmail.com>
2024-01-11 21:41:28 +0300

    fix(forms): custom error state matcher on dirty state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig, importProvidersFrom } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
 
-import { routes } from "./app.routes";
-import { environment } from "../environments/environment";
+import { routes } from './app.routes';
+import { CustomErrorStateMatcher } from './errors/custom-error-state-matcher';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebaseConfig))),
     importProvidersFrom(provideAuth(() => getAuth())),
-    importProvidersFrom(provideFirestore(() => getFirestore()))
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    { provide: ErrorStateMatcher, useClass: CustomErrorStateMatcher }
   ]
 };
